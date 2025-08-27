20ab351b81cae02f827e43e5bc69a181aaca42e3
Meaza Misgina <misginameaza@gmail.com>
2025-08-27 08:53:46 +0300

    fix: fixed import error

--- moovie/src/app/page.tsx
+++ moovie/src/app/page.tsx
@@ -1,9 +1,13 @@
-import Image from "next/image";
+'use client';
 import NavBar from "./sharedcomponents/NavBar";
+import Footer from "./sharedcomponents/Footer";
+
 export default function Home() {
   return (
     <div>
-      <NavBar/>
+      <NavBar onSearch={() => {}} />
+        <Footer/>
+
     </div>
   )
 };
